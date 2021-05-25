5d8dfedd9bc1a321a473caccd72f8cf45d8857d2
nitudeka <iamnitudeka@gmail.com>
2021-05-25 13:42:15 +0530

    added display

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ function App() {
   return (
     <div className="h-screen w-screen bg-white flex items-center justify-center">
       <div className="p-4 bg-black rounded-xl">
+        <div className="text-white h-14 flex px-2 items-center justify-end bg-gray-700 mb-4 rounded bg-opacity-50">
+          <p className="text-4xl">0</p>
+        </div>
         <div className="grid grid-cols-4 gap-4">
           <Button label="AC" extraLarge />
           <Button label="/" withAccent />
